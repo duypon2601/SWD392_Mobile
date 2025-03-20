29fb1b71afda17b2be6dfd01f29f04d3660a8135
Nam <169059967+DuongPhuongNam@users.noreply.github.com>
2025-03-20 21:11:36 +0700

    Update login view

--- lib/resources/form_field_widget.dart
+++ lib/resources/form_field_widget.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:hotpot/resources/color_manager.dart';
 import 'package:hotpot/resources/reponsive_utils.dart';
 
-// Enhanced FormFieldWidget with prefix icon support
+
+// ignore: must_be_immutable
 class FormFieldWidget extends StatelessWidget {
-  FormFieldWidget({
-      super.key,
+  FormFieldWidget(
+      {super.key,
       this.focusNode,
       this.icon,
       this.errorText = "",
@@ -19,7 +19,6 @@ class FormFieldWidget extends StatelessWidget {
       this.initValue,
       this.padding = 0,
       this.suffixIcon,
-      this.prefixIcon,
       this.enableInteractiveSelection = true,
       this.styleInput = const TextStyle(color: Colors.black),
       this.radiusBorder = 0,
@@ -31,11 +30,9 @@ class FormFieldWidget extends StatelessWidget {
       this.isCenter = false,
       this.focusColor,
       this.listFormat});
-      
   final FocusNode? focusNode;
   final Icon? icon;
   final Widget? suffixIcon;
-  final Widget? prefixIcon;
   String? errorText;
   final String? labelText;
   final TextEditingController? controllerEditting;
@@ -62,7 +59,7 @@ class FormFieldWidget extends StatelessWidget {
     return TextFormField(
       inputFormatters: listFormat,
       textDirection: directLTR ? TextDirection.ltr : TextDirection.rtl,
-      cursorColor: ColorsManager.primary,
+      cursorColor: Colors.black,
       textAlign: isCenter ? TextAlign.center : TextAlign.start,
       style: styleInput,
       enableInteractiveSelection: enableInteractiveSelection,
@@ -73,7 +70,7 @@ class FormFieldWidget extends StatelessWidget {
       decoration: InputDecoration(
         labelStyle: TextStyle(
             color: borderColor,
-            fontSize: UtilsReponsive.formatFontSize(14, context)),
+            fontSize: UtilsReponsive.formatFontSize(18, context)),
         fillColor: fillColor,
         filled: true,
         contentPadding: EdgeInsets.symmetric(
@@ -83,10 +80,7 @@ class FormFieldWidget extends StatelessWidget {
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(
               UtilsReponsive.height(radiusBorder ?? 20, context)),
-          borderSide: BorderSide(
-            color: focusColor ?? ColorsManager.primary, 
-            width: 1.5
-          ),
+          borderSide: BorderSide(color: focusColor ?? borderColor, width: 2),
         ),
         border: OutlineInputBorder(
           borderSide: BorderSide(
@@ -105,14 +99,11 @@ class FormFieldWidget extends StatelessWidget {
           borderRadius: BorderRadius.circular(
               UtilsReponsive.height(radiusBorder ?? 20, context)),
         ),
+        // labelText: labelText,
         hintText: labelText,
         hintTextDirection: TextDirection.ltr,
         hintMaxLines: 3,
-        hintStyle: TextStyle(
-          color: Colors.grey[400],
-          fontSize: UtilsReponsive.formatFontSize(14, context),
-        ),
-        prefixIcon: prefixIcon ?? icon,
+        prefixIcon: icon,
         suffixIcon: suffixIcon,
       ),
       keyboardType: textInputType,
